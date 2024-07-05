539ff7ffc35f17462fa04e2df1662ac03a1db27e
Mac <mac@MacBook-Pro-de-Mac.local>
2024-07-05 12:53:53 -0300

    feat: add initial files at backend

---
+++ backend/src/main/java/com/backend/recipeIdeal/models/Recipe.java
@@ -0,0 +1,29 @@
+package com.backend.recipeIdeal.models;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "recipe_seq", sequenceName = "recipe_sequence", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedTime;
+}
---
+++ backend/src/test/java/com/backend/recipeIdeal/RecipeIdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.recipeIdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeIdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
